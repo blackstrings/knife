cc7a3e099066d107e4058fbf861f2c352a9ad868
Xai <xlao@menards.net>
2016-05-28 22:47:41 -0500

    testSingleton with flip box

---
+++ KNIFProj/Assets/Main/Scripts/TempSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using LAO.Generic;
+
+public class TempSingleton : Singleton<TempSingleton> {
+
+    public GameObject activeGO { get; set; }
+
+	public GameObject getActiveGO() {
+        if (activeGO) {
+            return activeGO;
+        }
+        return null;
+    }
+
+}
