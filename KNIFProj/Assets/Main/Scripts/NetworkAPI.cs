045f3d1a6c88e2c58f2d69b6c681c7dfbf35d8a8
blackstrings <laoxai@MacBookPro2016.local>
2018-03-29 00:20:48 -0500

    Network login communication coming back from rails working + upgrade to unity 2017

---
+++ KNIFProj/Assets/Main/Scripts/NetworkAPI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Rainkey.Network;
+
+public class NetworkAPI : MonoBehaviour {
+
+	public Networker networker;
+
+	//buttons
+	public Text username;
+	public Text password;
+	public Button loginBtn;
+
+	// Use this for initialization
+	void Start () {
+		// fat arrow method
+//		loginBtn.onClick.AddListener (() => {
+//			networker.login(username.text, password.text);
+//		});
+
+		// simpler method
+		loginBtn.onClick.AddListener (login);
+	}
+	
+	private void login(){
+		if (networker) {
+			networker.login(username.text, password.text);
+		}
+	}
+
+//	public void updateWeapon(Weapon weapon){
+//		networker.updateWeapon (weapon);
+//	}
+
+	// TODO may not need
+	public LoginDTO getLoginDTO(){
+		LoginDTO dto = null;
+		if(networker){
+			dto = networker.getLoginDTO ();
+		}
+		return dto;
+	}
+}
---
+++ KNIFProj/Assets/Main/Scripts/Networker.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+namespace Rainkey.Network {
+	
+	public class Networker : MonoBehaviour
+	{
+	
+		private string domain;
+		private string loginUri;
+		private string weaponUri;
+		private LoginDTO loginDTO;	// stores the loginDTO
+
+		// Use this for initialization
+		void Start ()
+		{
+			domain = "http://weapons.herokuapp.com/";
+			loginUri = domain + "api/v1/login/";
+			weaponUri = domain + "api/v1/weapons/";
+		}
+
+		public void updateWeapon(string weapon){
+			Debug.Log ("weapon udated " + weapon);
+		}
+
+		public LoginDTO getLoginDTO(){
+			return loginDTO;
+		}
+
+		public void login(string username, string password) {
+			if (username != null && password != null) {
+				StartCoroutine (userLogin (username, password));
+			} else {
+				Debug.Log ("login failed, username or password is null");
+			}
+		}
+
+		private IEnumerator userLogin (string username, string password)
+		{
+
+			// fill the form
+			WWWForm f = new WWWForm ();
+			f.AddField ("email", username);	// yeng@email.com
+			f.AddField ("password", password);
+
+			// prepare the request
+			UnityWebRequest www = UnityWebRequest.Post (loginUri, f);
+			www.downloadHandler = new DownloadHandlerBuffer ();
+
+			// send the request
+			yield return www.SendWebRequest ();
+
+			// check the request
+			if (www.isNetworkError) {
+				Debug.Log (www.error);
+			} else {
+				// textOutput_gui.text = "www processing complete";
+				Debug.Log ("Form uploaded complete");
+
+				// the send back information will be available in the downloadhandler text
+				Debug.Log (www.downloadHandler.text);
+
+				//convert deserialized json str arrays, into tru objects
+				string returnedJsonData = www.downloadHandler.text;
+				//Debug.Log(www.downloadHandler.text);
+                
+				// list deserialize
+				//List<Foo> foos = JsonConvert.DeserializeObject<List<Foo>>(returnedJsonData);
+
+				// single object deserialize
+				loginDTO = JsonConvert.DeserializeObject<LoginDTO>(returnedJsonData);
+				Debug.Log("deserialize complete: " + loginDTO.auth_token);
+//                Debug.Log("converting json into tru objects >> now testing read from customer 1");
+//                Foo f = foos[0];
+//                Debug.Log("#Name: " + f.full_name 
+//                    + " #id: " + f.id
+//                    + " #email: " + f.email
+//                    + " #phone: " + f.phone	
+//                    );
+
+			}
+		}
+
+	}
+
+}
